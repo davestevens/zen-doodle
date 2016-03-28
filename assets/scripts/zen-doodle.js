1b76a9be0c89215f915e6b8d3b76662e647668cd
Dave Stevens <dave@dstvns.co.uk>
2016-03-28 11:42:52 +0100

    Moves Rendering into ZenDoodle.

--- assets/scripts/zen-doodle.js
+++ assets/scripts/zen-doodle.js
@@ -1,5 +1,17 @@
 import Shape from "./shape";
 
-module.exports = {
-  Shape: Shape
+class ZenDoodle {
+  constructor({ shapes, svg }) {
+    this.svg = svg;
+    this.shapes = shapes;
+  }
+
+  render() {
+    this.shapes.map((shapeConfig) => {
+      let shape = new Shape(shapeConfig);
+      this.svg.appendChild(shape.draw());
+    });
+  }
 }
+
+module.exports = ZenDoodle;
